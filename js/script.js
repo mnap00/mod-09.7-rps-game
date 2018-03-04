e16801e170ee9373ffc725af7957c5c55810e147
mnap <learn@10g.pl>
2018-03-04 19:33:22 +0100

    Add game's initiation scripts

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // 'New game' button listener
 var newGameBtn = document.getElementById('js-newGameButton');
+
 newGameBtn.addEventListener('click', newGame);
 
 // Player's 'Pick' buttons listeners
@@ -12,4 +13,50 @@ pickPaper.addEventListener('click', function() { playerPick('paper'); });
 pickScissors.addEventListener('click', function() { playerPick('scissors'); });
 
 // Game's logic
+var gameState = 'notStarted',  // started  // ended
+    player = {
+        name: '',
+        score: 0
+    },
+    computer = {
+        score: 0
+    };
 
+var newGameElem = document.getElementById('js-newGameElement'),
+    pickElem = document.getElementById('js-playerPickElement'),
+    resultsElem = document.getElementById('js-resultsTableElement');
+
+function setGameElements() {
+    switch(gameState) {
+        case 'started':
+            newGameElem.style.display = 'none';
+            pickElem.style.display = 'block';
+            resultsElem.style.display = 'block';
+            break;
+        case 'ended':
+            newGameBtn.innerText = 'Once again';
+        case 'notStarted':
+        default:
+            newGameElem.style.display = 'block';
+            pickElem.style.display = 'none';
+            resultsElem.style.display = 'none';
+    }
+}
+
+setGameElements();
+
+var playerPointsElem = document.getElementById('js-playerPoints'),
+    playerNameElem = document.getElementById('js-playerName'),
+    computerPointsElem = document.getElementById('js-computerPoints');
+
+function newGame() {
+    player.name = prompt('Please enter your name', 'player\'s name');
+    if (player.name) {
+        player.score = computer.score = 0;
+        gameState = 'started';
+        setGameElements();
+
+        playerNameElem.innerHTML = player.name;
+        // setGamePoints();  // This function has not been created yet
+    }
+}
